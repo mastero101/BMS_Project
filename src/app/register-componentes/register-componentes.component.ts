f2a6b5a643da4a10a6866f1a0fac112ff52bc885
mastero <castro.alejandro17@gmail.com>
2023-05-06 13:24:02 -0600

    Edit register-component add new function for upd

--- src/app/register-componentes/register-componentes.component.ts
+++ src/app/register-componentes/register-componentes.component.ts
@@ -11,6 +11,7 @@ export class RegisterComponentesComponent implements OnInit {
   selectedOption2: string = "";
   modelo: any;
   precio: any;
+  precioUpdate: any;
   tienda: any;
   id: any;
   id2: any;
@@ -49,6 +50,29 @@ export class RegisterComponentesComponent implements OnInit {
       });
   }
 
+  crudsUpdate() {
+    if (!this.selectedOption || !this.modelo || !this.precio || !this.selectedOption2) {
+      console.log("Por favor, complete todos los campos");
+      alert("Por favor, complete todos los campos")
+      return;
+    }
+  
+    axios
+      .put('https://nodemysql12.duckdns.org:443/', {
+        tipo: this.selectedOption,
+        modelo: this.modelo,
+        precio: this.precioUpdate,
+        tienda: this.selectedOption2,
+      })
+      .then(function (response) {
+        console.log(response);
+        location.reload();
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  }
+
 
   modelos() {
     this.modelo = (<HTMLInputElement>document.getElementById('modelo')).value;
@@ -58,6 +82,11 @@ export class RegisterComponentesComponent implements OnInit {
   precios() {
     this.precio = (<HTMLInputElement>document.getElementById('precio')).value;
     console.log(this.precio);
+  }
+
+  preciosUpdate() {
+    this.precioUpdate = (<HTMLInputElement>document.getElementById('precioUpdate')).value;
+    console.log(this.precioUpdate);
   }
 
   tiendas() {
